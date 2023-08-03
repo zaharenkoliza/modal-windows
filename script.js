5965a0de9e3f5e41ce23cd0a944b5bf0da8539d4
zaharenkoliza <Liza93087@gmail.com>
2023-08-03 19:29:23 +0700

    add comments

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 'use strict';
 
+// массив с кнопками
 const btns = document.querySelectorAll('.show-modal-window');
-console.log(btns);
 
+// метод, скрывающий или показывающий модальное окно
 const closeOrOpenWindow = function(){
    document.querySelector('.modal-window').classList.toggle('hidden');
    document.querySelector('.overlay').classList.toggle('hidden');
 }
 
+// цикл, проходящий по кнопкам из массива
 for (let i = 0; i < 3; i++){
+   // открывает модальное окно
    btns[i].addEventListener('click', closeOrOpenWindow)
+   // закрывает модальное окно
    document.querySelector('.close-modal-window').addEventListener('click', closeOrOpenWindow)
    document.querySelector('.overlay').addEventListener('click', closeOrOpenWindow)
-   console.log(btns[i]);
 }
 
+// при нажатии на Escape модальное окно закрывается
 document.addEventListener('keydown', function(e){
    if (e.key === 'Escape'){
       closeOrOpenWindow();
